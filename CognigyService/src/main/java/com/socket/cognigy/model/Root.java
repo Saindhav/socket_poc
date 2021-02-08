1d119dd92a0823f6debb338d6c2225aa52580925
SAINDHAV <SAINDHAV@LAPTOP-R33SQ4QE>
2021-02-08 18:04:26 +0530

    Removed Root

--- CognigyService/src/main/java/com/socket/cognigy/model/Root.java
+++
@@ -1,9 +0,0 @@
-package com.socket.cognigy.model;
-
-public class Root{
-    public String text;
-    public Data data;
-    public String traceId;
-    public boolean disableSensitiveLogging;
-    public String source;
-}
